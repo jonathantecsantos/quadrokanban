68fb6c60dd02ba9db4abc5b50c0dbb95688b9554
Jonathan Santos <jonathan.tecsantos@gmail.com>
2026-09-09 09:26:31 -0300

    chore: adicionar .gitignore e remover node_modules e arquivos .env do controle de versao

--- backend/dist/prisma.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-// Instância única (Singleton) do PrismaClient para evitar múltiplas conexões
-exports.prisma = new client_1.PrismaClient();
