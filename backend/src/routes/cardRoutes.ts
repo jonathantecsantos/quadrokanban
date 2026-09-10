e041e8fbf642f430c7f72982fcbcde3a62995dc0
Jonathan Santos <jonathan.tecsantos@gmail.com>
2026-09-10 00:26:34 -0300

    fix(backend): adicionar tipagens explicitas e mover @types para dependencies no deploy

--- backend/src/routes/cardRoutes.ts
+++ backend/src/routes/cardRoutes.ts
@@ -1,13 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { cardController } from '../controllers/cardController';
 
 const router = Router();
 
-router.get('/', (req, res, next) => cardController.getAll(req, res, next));
-router.get('/:id', (req, res, next) => cardController.getById(req, res, next));
-router.post('/', (req, res, next) => cardController.create(req, res, next));
-router.put('/:id', (req, res, next) => cardController.update(req, res, next));
-router.patch('/:id/move', (req, res, next) => cardController.move(req, res, next));
-router.delete('/:id', (req, res, next) => cardController.delete(req, res, next));
+router.get('/', (req: Request, res: Response, next: NextFunction) => cardController.getAll(req, res, next));
+router.get('/:id', (req: Request, res: Response, next: NextFunction) => cardController.getById(req, res, next));
+router.post('/', (req: Request, res: Response, next: NextFunction) => cardController.create(req, res, next));
+router.put('/:id', (req: Request, res: Response, next: NextFunction) => cardController.update(req, res, next));
+router.patch('/:id/move', (req: Request, res: Response, next: NextFunction) => cardController.move(req, res, next));
+router.delete('/:id', (req: Request, res: Response, next: NextFunction) => cardController.delete(req, res, next));
 
 export { router as cardRoutes };
+
